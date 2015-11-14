e3948f51ec5cf0a14404e18213b49e8540d5dac3
Josh Rahm <joshuarahm@gmail.com>
2015-11-14 13:12:50 -0700

    more of the infrastructure

--- src/Hiss.hs
+++ src/Hiss.hs
@@ -4,8 +4,23 @@ import Data.Set (Set)
 import Hiss.Monad
 import Hiss.Flags
 
+import Language.Python.Version2.Parser as P2
+import Language.Python.Version3.Parser as P3
+
+import Language.Python.Common
+
+parsePython :: FilePath -> Hiss (ModuleSpan, [Token])
+parsePython fp = do
+    version2 <- isVersion2
+    sourceCode <- hissLiftIO (readFile fp)
+    fromEither $
+        (if version2 then P2.parseModule else P3.parseModule) fp sourceCode
+
+
 runHissM :: FilePath -> Hiss ()
-runHissM = undefined
+runHissM fp = do
+    ast <- parsePython fp
+    hlog (show ast)
 
 runHissOnOneFile :: Set Flag -> FilePath -> IO ()
-runHissOnOneFile flags file = runHiss flags (runHissM file)
+runHissOnOneFile flags file = runHissIO flags (runHissM file)
--- src/Hiss/Monad.hs
+++ src/Hiss/Monad.hs
@@ -1,26 +1,55 @@
-module Hiss.Monad (Hiss, runHiss) where
+module Hiss.Monad (Hiss, runHiss, hlog, verbose, isVersion2, hasFlag,
+                   die, fromEither, hissLiftIO, runHissIO) where
 
 import Control.Monad.Trans.State.Lazy
 import Control.Monad.Trans (lift)
+import Control.Monad.Trans.Either
 
 import Control.Monad (when)
 
 import Hiss.Flags
 import Data.Set (Set)
 
+import System.IO
+import System.Exit (exitWith, ExitCode(ExitFailure))
+
 data HissState = HissState {
     flags :: Set Flag
 }
 
-type Hiss = StateT HissState IO
+type Hiss = EitherT String (StateT HissState IO)
 
-runHiss :: Set Flag -> Hiss a -> IO a
-runHiss flags fn = evalStateT fn (HissState flags)
+hissLiftIO :: IO a -> Hiss a
+hissLiftIO = lift . lift
+
+runHiss :: Set Flag -> Hiss a -> IO (Either String a)
+runHiss flags fn = evalStateT (runEitherT fn) (HissState flags)
+
+runHissIO :: Set Flag -> Hiss a -> IO a
+runHissIO flags fn = do
+    either <- evalStateT (runEitherT fn) (HissState flags)
+    case either of
+        Left s -> hPutStrLn stderr s >> exitWith (ExitFailure 1)
+        Right a -> return a
 
 hlog :: String -> Hiss ()
-hlog str = lift $ putStrLn str
+hlog str = lift $ lift $ putStrLn str
 
 verbose :: String -> Hiss ()
 verbose str = do
-    isVerbose <- (Verbose `elem`) . flags <$> get
+    isVerbose <- (Verbose `elem`) . flags <$> lift get
     when isVerbose $ hlog str
+
+hasFlag :: Flag -> Hiss Bool
+hasFlag f = (f `elem`) . flags <$> lift get
+
+isVersion2 :: Hiss Bool
+isVersion2 = hasFlag Version2
+
+fromEither :: (Show s) => Either s a -> Hiss a
+fromEither e = case e of
+    Left s -> die (show s)
+    Right a -> return a
+
+die :: String -> Hiss a
+die = left
