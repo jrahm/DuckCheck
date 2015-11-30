de9159f5c96605382f7424e989867d3ba02237b8
Josh Rahm <joshuarahm@gmail.com>
2015-11-30 12:55:51 -0700

    Int type builtin added

--- src/DuckTest/Builtins.hs
+++ src/DuckTest/Builtins.hs
@@ -8,7 +8,7 @@ initState =
     addVariableType "print" (Functional [("", anyType)] anyType) $
     addVariableType "len" (Functional [("", Scalar $ fromList ["__len__"])] anyType) $
     addVariableType "str" (Functional [("", Scalar $ fromList ["__str__"])] strType) $
-    addVariableType "int" (Functional [("", anyType)] anyType)
+    addVariableType "int" (Functional [("", anyType)] intType)
     emptyState
 --
 -- builtinGlobalFunctions :: Map String Function
@@ -33,6 +33,29 @@ initState =
 
 strType :: PyType
 strType = Scalar $ setTypeName "str" $ fromList stringAttrs
+
+intType :: PyType
+intType = Scalar $ setTypeName "int" $ fromList intAttrs
+
+intAttrs :: [String]
+intAttrs =
+    ["__abs__", "__add__", "__and__", "__bool__",
+     "__ceil__", "__class__", "__delattr__", "__dir__", "__divmod__",
+     "__doc__", "__eq__", "__float__", "__floor__",
+     "__floordiv__", "__format__", "__ge__", "__getattribute__",
+     "__getnewargs__", "__gt__", "__hash__", "__index__",
+     "__init__", "__int__", "__invert__", "__le__",
+     "__lshift__", "__lt__", "__mod__", "__mul__",
+     "__ne__", "__neg__", "__new__", "__or__",
+     "__pos__", "__pow__", "__radd__", "__rand__",
+     "__rdivmod__", "__reduce__", "__reduce_ex__", "__repr__",
+     "__rfloordiv__", "__rlshift__", "__rmod__", "__rmul__",
+     "__ror__", "__round__", "__rpow__", "__rrshift__",
+     "__rshift__", "__rsub__", "__rtruediv__", "__rxor__",
+     "__setattr__", "__sizeof__", "__str__", "__sub__",
+     "__subclasshook__", "__truediv__", "__trunc__", "__xor__",
+     "bit_length", "conjugate", "denominator", "from_bytes",
+     "imag", "numerator", "real", "to_bytes"]
 
 stringAttrs :: [String]
 stringAttrs = ["__add__", "__class__", "__contains__",
